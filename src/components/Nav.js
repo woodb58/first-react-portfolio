1e4c83e2a7c5f1e25f9b97137aa9a414f3a1b7b6
woodb58 <woodb58@gmail.com>
2022-07-18 14:58:21 -0500

    created navbar/made nav responsive with transition

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,31 +1,40 @@
-import "./NavStyles.css"
-
-import React from 'react'
-import { Link } from "react-router-dom"
+import "./NavStyles.css";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-  return (
-    <div className="header">
-        <Link to={"/"}>
-          <h1>Portfolio</h1>
-          <ul className="nav-links">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </Link>
-        
-    </div>
-  )
-}
+  const [click, unClick] = useState(false);
+  const handleClick = () => unClick(!click);
 
-export default Navbar
+  return (
+    <div className="nav-bar">
+      <Link to={"/"}>
+        <h1>Portfolio</h1>
+      </Link>
+      <ul className={click ? "nav-links active" : "nav-links"}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "white" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "white" }} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
