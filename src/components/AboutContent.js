54430282eed9b524c77d59107cec59039bb036ee
woodb58 <woodb58@gmail.com>
2022-07-19 23:28:27 -0500

    styled about section

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "./AboutContentStyles.css";
 import { Link } from "react-router-dom";
-import MePic from "../assets/me4.png"
+import MePic from "../assets/me4.png";
 
 const AboutContent = () => {
   return (
     <div className="about">
-     <img src={MePic} alt="me pic"></img>
-      <h1>About</h1>
-      <p>about me and such</p>
+      <img src={MePic} alt="me pic"></img>
+      <h1>Brian Wood</h1>
+      <p>
+        Your friendly neighborhood Web Developer. There will be more words here
+        at some point I assume but right now that's all i have for you. Feel
+        free to hit the contact button below, and fill out the form... the form
+        doesn't work right now but it's the thought that counts!{" "}
+      </p>
       <Link to="/contact">
         <button className="btn"> Contact Me</button>
       </Link>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import "./HeroStyles.css";
 import React from "react";
 import { Link } from "react-router-dom";
 import HomeImg from "../assets/creativity.jpg";
-import MePic from "../assets/me2.png";
+
 const Hero = () => {
   return (
     <div className="hero">
@@ -10,7 +10,6 @@ const Hero = () => {
         <img className="home-img" src={HomeImg} alt="coffee pic" />
       </div>
       <div className="content">
-        <img src={MePic} />
         <h2>Hi! I'm Brian</h2>
         <h1>Full Stack Developer</h1>
         <div>
